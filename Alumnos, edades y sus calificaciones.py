a61a2ade465c2bd8b1c8230423e41890026309a8
Faithy <maximinofort@gmail.com>
2025-10-02 13:19:29 -0600

    Input con while creado

--- Alumnos, edades y sus calificaciones.py
+++ Alumnos, edades y sus calificaciones.py
@@ -7,7 +7,7 @@
 #calificaciones
 #edades
 #Muestre:
-#Todos los nombres en una sola línea separados por comas.
+    #Todos los nombres en una sola línea separados por comas.
     #El alumno con la mejor calificación y el alumno con la peor calificación.
     #El promedio de calificaciones (redondeado a 2 decimales).
     #El promedio de edades.
@@ -23,40 +23,46 @@ Edades = []
 Calificaciones = []
 Errores = []
 while True:
-    datos = input("Ingresa los nombres de los alumnos seguidos de su calificacion y por último su edad, todos los datos deben estar separados por espacios: ")
+    datos = input("Ingresa los nombres de los alumnos seguidos de su calificacion y por último su edad.\nTodos los datos deben estar separados por espacios: ")
     DatosListados = datos.split()
     if (len(DatosListados) / 3) == (int(len(DatosListados) / 3)):
         for x in range(len(DatosListados)):
-            try:
+           
                 if ((x + 3) / 3) == int(((x + 3) / 3)):
                     try:
                         int(DatosListados[x])
-                        print("Error en el dato número",x, "\nLos valores númericos no son validos como nombres")
+                        Errores.append(x)
+                        print("Error en el dato número",(x + 1), "\nLos valores númericos no son validos como nombres")
                     except ValueError:
                         Nombres.append(x)
-
                 elif ((x + 2) / 3) == int((x + 2) / 3):
-                       if DatosListados[x] == abs(DatosListados[x]):
-                            Calificaciones.append(DatosListados[x])
-                            print("Calificaciones son: ", DatosListados[x])
-                       else:
-                            print("Error en el dato número",x, "\nLos valores negativos no son validos como calificaciones")
-
+                        try:
+                            DatosListados[x] = int(DatosListados[x])
+                            if DatosListados[x] == abs(DatosListados[x]):
+                                 Calificaciones.append(DatosListados[x])
+                            else:
+                                print("Error en el dato número",(x + 1), "\nLos valores negativos no son validos como calificaciones")
+                        except ValueError:
+                            Errores.append(x)
                 else:
-                    if DatosListados[x] == abs(DatosListados[x]):
-                        Edades.append(DatosListados[x])
-                        print("Edades son: ", DatosListados[x])
-                    else:
-                        print("Error en el dato número",x, "\nLos valores negativos no son validos como edades")
-                    
-            except TypeError:
-                Errores.append(x)
-        break
+                    try:
+                        DatosListados[x] = int(DatosListados[x])
+                        if DatosListados[x] == abs(DatosListados[x]):
+                            Edades.append(DatosListados[x])
+                        else:
+                             print("Error en el dato número",x, "\nLos valores negativos no son validos como edades")
+                    except ValueError:
+                        Errores.append(x)
     else:
         print("Los datos estan incompletos, deben de poder crearse una cantidad entera de trios de datos (Nombre, Calificacion, Edad). Intente nuevamente.")
-if len(Errores) > 0:
-    print("Se a introducido un tipo de dato incorrecto en los datos numeros: ", ",".join(str(x + 1) for x in Errores))
-
-
-
-
+    if len(Errores) > 0:
+        print("Se a introducido un tipo de dato incorrecto en los datos numeros: ", ", ".join(str(x + 1) for x in Errores))
+        Errores = []
+        Nombres = []
+        Edades = []
+        Numeros = []
+    else:
+            break
+print("Alumnos ingresados: ", ", ".join(str(x)for x in Nombres))
+for x in reversed(range(sorted(Calificaciones))):
+     print(Calificaciones[x])
