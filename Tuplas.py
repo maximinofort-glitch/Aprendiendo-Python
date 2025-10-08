5447d90247807208b5f61ea5cbef1cfbcd303c59
faithy <maximinofort@gmail.com>
2025-10-07 19:33:34 -0600

    avanzando en las tuplas (ya entiendo mucho mejor)

--- Tuplas.py
+++ Tuplas.py
@@ -13,36 +13,56 @@ Errores = []
 Contador_Tuplas = 1
 Alumnos = []
 while True:
-    Datos = input("Ingresa los datos de los alumnos (nombre, edad, calificación) separados por espacios: ")
-    #Datos = "max 22 8.7 sofia 21 9 tola 22 4.5"
+    #Datos = input("Ingresa los datos de los alumnos (nombre, edad, calificación) separados por espacios: ")
+    Datos = "max 22 8.7 sofia 21 9 tola 22 4.5"
     if len(Datos.split()) % 3 != 0:
             print("Error: Los datos estan incompletos, deben de poderse crear conjuntos de tres (Nombre, Edad, Calificacion), intenta nuevamente")
             continue
     else:
-        for x in range(0, len(Datos.split()), 3):
-            Nombre = Datos[x]
-            Edad = Datos[x + 1]
-            Calificacion = Datos[x + 2]
+        DatosListados = Datos.split()
+        for x in range(0, len(DatosListados), 3):
+            Errores = []
+            Nombre = DatosListados[x]
+            Edad = DatosListados[x + 1]
+            Calificacion = DatosListados[x + 2]
+            print(Edad)
+            print(Calificacion)
             try:
                 Edad = int(Edad)
             except ValueError:
                 print(Edad)
                 Errores.append("si")
-                print("Error: Se ha introducido un tipo de dato incorrecto. La edad debe ser un número entero.")
+                print("Error: Se ha introducido un tipo de dato incorrecto en la posicion ",(x + 1),". La edad debe ser un número entero.")
             try:
-                Calificacion = float(Calificacion)
+                Calificacion = float(Calificacion) 
             except ValueError:
                 Errores.append("Si")
-                print("Error: La calificación debe ser un número decimal.")      
-            if len(Errores < 1):
-                ConjuntoDe3 = (Nombre, Edad, Calificacion)
-                Alumnos.append(ConjuntoDe3)
+                print("Error: Se ha introducido un tipo de dato incorrecto en la posicion ",(x + 2),". La calificación debe ser un número decimal.")      
+            Conjunto = (Nombre, Edad, Calificacion)
+            Alumnos.append(Conjunto)
         if len(Errores) > 0:
-            Erorres = []
             continue
-
         break
     
 print("funciona")
-    
-print(ConjuntoDe3)
+CalificacionesMax = []
+CalificacionesMin = []
+for Nombre, Edad, Calificacion in Alumnos:
+    print(f"{Nombre}, {Edad} años, calificación: {Calificacion}")
+    Calificaciones.append(Calificacion)
+for x in Calificaciones:
+    if x == sorted(Calificaciones)[len(Calificaciones) - 1]
+        CalificacionMax.append(x)
+    elif x == sorted(Calificaciones)[0]
+        CalificacionesMin.append(X)
+if len(CalificacionesMax) == 1
+
+elif len(CalificacionesMax) > 2
+
+else:
+
+if len(CalificacionesMin) == 1
+
+elif len(CalificacionesMin) > 2
+
+else:
