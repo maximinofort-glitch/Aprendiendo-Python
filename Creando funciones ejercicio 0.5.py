d4519de4b82d2453844d82128d052cc270b08120
faithy <maximinofort@gmail.com>
2025-10-15 14:48:54 -0600

    casi no hice nada pero ay esta practicamente acabado, lo siguiente sera otro ejercicio

--- Creando funciones ejercicio 0.5.py
+++ Creando funciones ejercicio 0.5.py
@@ -1,25 +1,26 @@
-#Mini ejercicio propuesto
+# sMini ejercicio propuesto
 
-#Haz una función llamada resumen_alumno que reciba:
+# Haz una función llamada resumen_alumno que reciba:
     #nombre (obligatorio)
     #edad (por defecto 18)
     #calificaciones (cualquier cantidad de números usando *args)
-#Y que devuelva:
+# Y que devuelva:
     #el promedio,
     #la mayor y la menor calificación.
+# max 22 10 9.7 7.9
 
-def resumen_alumno(Nombre, Edad = "18",*Calificaciones):
-    Calificaciones = list(Calificaciones)
-    Promedio = sum(Calificaciones) / len(Calificaciones)
+def resumen_alumno(Nombre, Edad = "18", *Calificaciones):
+    Calificaciones = [float(x) for x in Calificaciones]
+    Promedio = (sum(Calificaciones)) / len(Calificaciones)
     MayorCalificacion = max(Calificaciones)
     MenorCalificacion = min(Calificaciones)
-    return print(promedio, MayorCalificacion, MenorCalificacion)
+    return print(round(Promedio), MayorCalificacion, MenorCalificacion)
+    
 while True:
     Datos = input("Escribe el nombre del alumno seguido por su edad y calificaciones: ")
-    print(Datos)
-    print(Datos.split())
-    if len(Datos.split()) % 3 == 0:
+    if len(Datos.split()) >= 3:
         print(resumen_alumno(*Datos.split()))
+
         break
     else:
         continue
