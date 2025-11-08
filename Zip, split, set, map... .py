64efe6ea344f9fdfab622b33ca7584583acc6579
faithy <maximinofort@gmail.com>
2025-11-07 19:08:27 -0600

    comenzare con unas cuantas funciones mas y al final comenzare a usar json y demas cosas de archivos al igual que las clases que ahora mismo solo se que se relacionan con las funciones

---
+++ Zip, split, set, map... .py
@@ -0,0 +1,8 @@
+#Arroz:20:True, Frijoles:15:False, Aceite:30:True, 
+
+datos = input("Introduce los objetos con su precio y disponibilidad con el formato producto:precio:True/False")
+datos_listados = datos.split(":")
+objetos = []
+
+objetos = [i for x in datos_listados if objetos.index(x) % 3 == 0]
+print(objetos)
